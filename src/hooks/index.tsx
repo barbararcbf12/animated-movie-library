21eb3bff0a9e2ed230f3f61a737f3e93f46c9be9
Barbara <barbaraferreira12@gmail.com>
2020-03-02 09:14:08 +0100

    Add pagination

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -4,32 +4,38 @@ import { MovieType } from '../@types/Movie'
 
 export const themoviedb = process.env.REACT_APP_THEMOVIEDB_APIKEY
 
-type MoviesRequest = {
-    url: string,
-    setMovies: React.Dispatch<React.SetStateAction<MovieType[] | undefined>>
+export const URL_DISCOVER = `https://api.themoviedb.org/3/discover/movie?api_key=${themoviedb}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false`
+const URL_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key="
+const language = "&language=en-US"
+
+type MovieRequest = {
+    URL_MOVIE: string,
+    setMovie: React.Dispatch<React.SetStateAction<MovieType | undefined>>
 }
 
-export async function fetchMovies({ url, setMovies }: MoviesRequest){
+export async function fetchMovie({ URL_MOVIE, setMovie }: MovieRequest){
     try {
-        const res = await fetch(url)
-        const movies = await res.json()
-        setMovies(movies.results)
+        const res = await fetch(URL_MOVIE)
+        const movie = await res.json()
+        setMovie(movie)
     } catch (e) {
         return <ErrorComponent />
     }
 }
 
-type MovieRequest = {
-    url: string,
-    setMovie: React.Dispatch<React.SetStateAction<MovieType | undefined>>
+type MoviesSearch = {
+    query: string,
+    pageNumber: number,
+    setMovies: React.Dispatch<React.SetStateAction<MovieType[] | undefined>>,
+    setPageNumber: React.Dispatch<React.SetStateAction<number>>,
+    setTotalPages: React.Dispatch<React.SetStateAction<number | null>>
 }
 
-export async function fetchMovie({ url, setMovie }: MovieRequest){
-    try {
-        const res = await fetch(url)
-        const movie = await res.json()
-        setMovie(movie)
-      } catch (e) {
-        return <ErrorComponent />
-      }
-}
+export function searchMovies({query, setMovies, pageNumber, setPageNumber, setTotalPages}: MoviesSearch) {
+    fetch(URL_SEARCH + `${themoviedb}` + language + "&query=" + query + "&page=" + pageNumber)
+      .then(res => res.json())
+      .then(json => {
+        setMovies(json.results)
+        setTotalPages(json.total_pages)
+      })
+  }
