e628ddab5d9fb8917ff0186d37136b3649ad10f3
Babi* <babi@Macbooks-MacBook-Pro.local>
2020-03-13 14:46:31 +0100

    Refactor api calls and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,17 @@ import {
   Switch
 }  from 'react-router-dom'
 import './App.css'
+import styled from 'styled-components'
 import MoviesList from './components/MoviesList'
 import MovieDetails from './components/MovieDetails'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+const Main = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
   return (
@@ -17,9 +24,11 @@ function App() {
         <Header />
         
         <Switch>
-            <Route exact path="/" component={MoviesList} />
-            <Route path="/:id" component={MovieDetails} />
-        </Switch>
+          <Main>
+              <Route exact path="/" component={MoviesList} />
+              <Route path="/:id" component={MovieDetails} />
+          </Main>
+        </Switch> 
 
         <Footer />
       </div>
