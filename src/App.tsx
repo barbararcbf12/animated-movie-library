26b79d54bc9bc049cd052f409922eb8e6b1b6dfd
Babi* <babi@Macbooks-MacBook-Pro.local>
2020-03-17 20:45:41 +0100

    Animate on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import React from 'react'
-import { 
-  BrowserRouter as Router, 
-  Route, 
-  Switch
-}  from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import styled from 'styled-components'
-import MoviesList from './components/MoviesList'
-import MovieDetails from './components/MovieDetails'
 import Header from './components/Header'
-import Footer from './components/Footer'
-
-const Main = styled.section`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`
+import Main from './components/Main'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        
-        <Switch>
-          <Main>
-              <Route exact path="/" component={MoviesList} />
-              <Route path="/:id" component={MovieDetails} />
-          </Main>
-        </Switch> 
-
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Header />
+          <Main />
+        </div>
+      </Router>
   )
 }
 
---
+++ src/components/Main.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Switch, useLocation }  from 'react-router-dom'
+import MoviesList from './MoviesList'
+import MovieDetails from './MovieDetails'
+import { animated, useTransition } from 'react-spring'
+import Footer from './Footer'
+
+const Main = () => {
+    const location: any = useLocation()
+
+    const transitions = useTransition(location, location => location.key, {
+        from: { opacity: 0, position: 'absolute', width: '100%', transform: 'translate3d(100%, 0, 0)' },
+        enter: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
+        leave: { opacity: 0, transform: 'translate3d(0, 100%, 0)' }
+    })
+    return (
+        <>
+            { transitions.map(({ item, key, props }) => ( item &&
+                <animated.div key={key} style={props}>
+                    <Switch location={item}>
+                        <Route exact path="/" component={MoviesList} />
+                        <Route path="/:id" component={MovieDetails} />
+                    </Switch> 
+                    <Footer />
+                </animated.div>
+            ))}
+        </>
+    )
+}
+
+export default Main
